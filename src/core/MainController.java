16898bf74be76279ed1da3fdfb01c5d6ae60fda0
SophYo <sophiya.dimitrova@gmail.com>
2019-07-30 23:19:13 +0300

    created + initial constructor

---
+++ src/core/MainController.java
@@ -0,0 +1,11 @@
+package core;
+
+import java.util.Scanner;
+
+public class MainController {
+    private Scanner scanner;
+
+    public MainController() {
+        this.scanner = new Scanner(System.in);
+    }
+}
