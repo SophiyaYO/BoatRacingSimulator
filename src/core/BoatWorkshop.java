6fcc5c70d6be4c9b4720508e2f80dc41cece7522
SophYo <sophiya.dimitrova@gmail.com>
2019-07-31 00:00:41 +0300

    finished

--- src/core/BoatWorkshop.java
+++ src/core/BoatWorkshop.java
@@ -1,7 +1,8 @@
 package core;
 
-import models.Boat;
-import models.Engine;
+import exceptions.ArgumentException;
+import exceptions.NonExistantModelException;
+import models.*;
 import repositories.Repository;
 
 public class BoatWorkshop implements BoatFactory {
@@ -13,7 +14,38 @@ public class BoatWorkshop implements BoatFactory {
     }
 
     @Override
-    public Boat produce(String[] args) {
-        return null;
+    public Boat produce(String[] args) throws ArgumentException, NonExistantModelException {
+        String typeContainer = args[0];
+
+        Boat boat = null;
+
+        if (typeContainer.contains("RowBoat")) {
+            boat = new RowBoat(args[1],
+                    Integer.parseInt(args[2]),
+                    Integer.parseInt(args[3]));
+
+        } else if (typeContainer.contains("SailBoat")) {
+
+            boat = new SailBoat(args[1],
+                    Integer.parseInt(args[2]),
+                    Integer.parseInt(args[3]));
+
+        } else if (typeContainer.contains("PowerBoat")) {
+
+            boat = new PowerBoat(args[1],
+                    Integer.parseInt(args[2]),
+                    this.engineRepository.get(args[3]),
+                    this.engineRepository.get(args[4]));
+
+        } else if (typeContainer.contains("Yacht")) {
+
+            boat = new Yacht(args[1],
+                    Integer.parseInt(args[2]),
+                    this.engineRepository.get(args[3]),
+                    Integer.parseInt(args[4]));
+
+        }
+
+        return boat;
     }
 }
