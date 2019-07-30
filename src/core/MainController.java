934c1ced0e91a3884b89905f479eee1cf012274e
SophYo <sophiya.dimitrova@gmail.com>
2019-07-30 23:27:57 +0300

    new field

--- src/core/MainController.java
+++ src/core/MainController.java
@@ -1,12 +1,16 @@
 package core;
 
+import models.Engine;
+
 import java.util.Scanner;
 
 public class MainController {
     private Scanner scanner;
+    private EngineFactory engineFactory;
 
-    public MainController() {
+    public MainController(EngineFactory engineFactory) {
         this.scanner = new Scanner(System.in);
+        this.engineFactory = engineFactory;
     }
 
     public void run() {
