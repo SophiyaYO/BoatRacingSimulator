28727f44b233f23c17701f7c84f6a0b22296da9e
SophYo <sophiya.dimitrova@gmail.com>
2019-07-30 17:15:41 +0300

    constructor + fields added

--- src/models/Engine.java
+++ src/models/Engine.java
@@ -1,4 +1,13 @@
 package models;
 
 public abstract class Engine {
+    private String model;
+    private int horsePower;
+    private int displacement;
+
+    public Engine(String model, int horsePower, int displacement) {
+        this.model = model;
+        this.horsePower = horsePower;
+        this.displacement = displacement;
+    }
 }
