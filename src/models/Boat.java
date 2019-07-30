4d7f27e30c6a8797c373e13e64fdeb8e97ff44b1
SophYo <sophiya.dimitrova@gmail.com>
2019-07-30 21:05:19 +0300

    getWeight() added

--- src/models/Boat.java
+++ src/models/Boat.java
@@ -5,50 +5,54 @@ import helpers.Validator;
 import models.interfaces.Speed;
 
 public abstract class Boat implements Comparable<Boat>, Speed {
-        private static  final int MIN_MODEL_LENGTH = 5;
-        private String model;
-        private int weight;
+    private static final int MIN_MODEL_LENGTH = 5;
+    private String model;
+    private int weight;
 
-        protected Boat(String model, int weight) throws ArgumentException {
-                this.setModel(model);
-                this.setWeight(weight);
+    protected Boat(String model, int weight) throws ArgumentException {
+        this.setModel(model);
+        this.setWeight(weight);
+    }
+
+    private void setWeight(int weight) throws ArgumentException {
+        if (Validator.validateParam(weight)) {
+            this.weight = weight;
+        } else {
+            throw new ArgumentException(Validator.generateErrorMessage("Weight"));
         }
+    }
 
-        private void setWeight(int weight) throws ArgumentException {
-                if (Validator.validateParam(weight)) {
-                        this.weight = weight;
-                } else {
-                        throw new ArgumentException(Validator.generateErrorMessage("Weight"));
-                }
+    protected int getWeight() {
+        return this.weight;
+    }
+
+    private void setModel(String model) throws ArgumentException {
+        if (Validator.validateBoatModel(model)) {
+            this.model = model;
+        } else {
+            throw new ArgumentException(
+                    "Model's name must be at least "
+                            + MIN_MODEL_LENGTH
+                            + " symbols long.");
         }
+    }
 
-        private void setModel(String model) throws ArgumentException {
-                if (Validator.validateBoatModel(model)) {
-                        this.model = model;
-                } else {
-                        throw new ArgumentException(
-                                "Model's name must be at least "
-                                        + MIN_MODEL_LENGTH
-                                        + " symbols long.");
-                }
-        }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null ||
+                getClass().getSuperclass()
+                        != o.getClass().getSuperclass()) return false;
 
-        @Override
-        public boolean equals(Object o) {
-                if (this == o) return true;
-                if (o == null ||
-                        getClass().getSuperclass()
-                                != o.getClass().getSuperclass()) return false;
+        Boat boat = (Boat) o;
 
-                Boat boat = (Boat) o;
+        return model.equals(boat.model);
+    }
 
-                return model.equals(boat.model);
-        }
-
-        @Override
-        public int hashCode() {
-                return model.hashCode();
-        }
+    @Override
+    public int hashCode() {
+        return model.hashCode();
+    }
 
     @Override
     public int compareTo(Boat o) {
