c9811ccc7e9f76d52b6bb18a9b9e8272ee43e122
SophYo <sophiya.dimitrova@gmail.com>
2019-07-30 20:49:48 +0300

    created + fields+ constructor + setDistance()

---
+++ src/models/Race.java
@@ -0,0 +1,36 @@
+package models;
+
+import exceptions.ArgumentException;
+import helpers.Validator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Race {
+    private int distance;
+    private int windSpeed;
+    private int currentSpeed;
+    private boolean allowMottorBoats;
+    private List<Boat> participants;
+
+
+    public Race(int distance, int windSpeed,
+                int currentSpeed, boolean allowMotorBoats)
+            throws ArgumentException {
+
+        this.setDistance(distance);
+        this.windSpeed = windSpeed;
+        this.currentSpeed = currentSpeed;
+        this.allowMottorBoats = allowMotorBoats;
+        this.participants = new ArrayList<>();
+    }
+
+    private void setDistance(int distance) throws ArgumentException {
+        if (Validator.validateParam(distance)) {
+            this.distance = distance;
+
+        } else {
+            throw new ArgumentException(Validator.generateErrorMessage("Distance"));
+        }
+    }
+}
