81aaed6d9f7965ef11f8960795cdf69a6b48d27b
SophYo <sophiya.dimitrova@gmail.com>
2019-07-30 14:52:17 +0300

    validateSailEfficiency() added

--- src/helpers/BoatValidator.java
+++ src/helpers/BoatValidator.java
@@ -9,4 +9,8 @@ public class BoatValidator {
     public static boolean validateParam(int param) {
         return param > 0;
     }
+
+    public static boolean validateSailEfficiency(int efiiciency) {
+        return efiiciency > 0 && efiiciency < 101;
+    }
 }
