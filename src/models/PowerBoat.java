60ef4d4582f2877a12f7da2d790722dc4e93e344
SophYo <sophiya.dimitrova@gmail.com>
2019-07-30 15:00:21 +0300

    fields added

--- src/models/PowerBoat.java
+++ src/models/PowerBoat.java
@@ -6,7 +6,11 @@ public class PowerBoat extends Boat {
     private Engine primaryEngine;
     private Engine secondaryEngine;
 
-    protected PowerBoat(String model, int weight) throws ArgumentException {
+    protected PowerBoat(String model, int weight,
+                        Engine primaryEngine, Engine secondaryEngine ) throws ArgumentException {
+
         super(model, weight);
+        this.primaryEngine = primaryEngine;
+        this.secondaryEngine = secondaryEngine;
     }
 }
