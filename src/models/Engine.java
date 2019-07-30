ac497002b9c6ab8f983dab9afb76428831646013
SophYo <sophiya.dimitrova@gmail.com>
2019-07-30 17:24:53 +0300

    setModel() and exceptions added

--- src/models/Engine.java
+++ src/models/Engine.java
@@ -1,13 +1,29 @@
 package models;
 
+import exceptions.ArgumentException;
+import helpers.Validator;
+
 public abstract class Engine {
+    private static  final int MIN_MODEL_LENGTH = 3;
+
     private String model;
     private int horsePower;
     private int displacement;
 
-    public Engine(String model, int horsePower, int displacement) {
-        this.model = model;
+    public Engine(String model, int horsePower, int displacement) throws ArgumentException {
+        this.setModel(model);
         this.horsePower = horsePower;
         this.displacement = displacement;
+    }
+
+    public void setModel(String model) throws ArgumentException {
+        if (Validator.validateEngine(model)) {
+            this.model = model;
+
+        } else {
+            throw new ArgumentException("Model's name must be at least "
+                    + MIN_MODEL_LENGTH
+                    + " symbols long.");
+        }
     }
 }
