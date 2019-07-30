ccc033698e157f32d3d6896fb5d9e56a0ccef50a
SophYo <sophiya.dimitrova@gmail.com>
2019-07-30 21:22:52 +0300

    calcSpeed(Race race) overrided

--- src/models/PowerBoat.java
+++ src/models/PowerBoat.java
@@ -7,10 +7,18 @@ public class PowerBoat extends Boat {
     private Engine secondaryEngine;
 
     public PowerBoat(String model, int weight,
-                        Engine primaryEngine, Engine secondaryEngine ) throws ArgumentException {
+                     Engine primaryEngine, Engine secondaryEngine) throws ArgumentException {
 
         super(model, weight);
         this.primaryEngine = primaryEngine;
         this.secondaryEngine = secondaryEngine;
+    }
+
+    @Override
+    public double calcSpeed(Race race) {
+        return (this.primaryEngine.getOutput()
+                + this.secondaryEngine.getOutput())
+                - this.getWeight()
+                + (race.getCurrentSpeed() / 5.0);
     }
 }
