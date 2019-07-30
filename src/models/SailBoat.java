d639e4899123f7eaafc73f1352833d63c285f550
SophYo <sophiya.dimitrova@gmail.com>
2019-07-30 14:52:31 +0300

    setSailEfficiency() added

--- src/models/SailBoat.java
+++ src/models/SailBoat.java
@@ -1,11 +1,22 @@
 package models;
 
 import exceptions.ArgumentException;
+import helpers.BoatValidator;
 
 public class SailBoat extends Boat {
 
+    private int sailEfficiency;
 
-    public SailBoat(String model, int weight) throws ArgumentException {
+    public SailBoat(String model, int weight, int sailEfficiency) throws ArgumentException {
         super(model, weight);
+        this.setSailEfficiency(sailEfficiency);
+    }
+
+    private void setSailEfficiency(int sailEfficiency) throws ArgumentException {
+        if (BoatValidator.validateSailEfficiency(sailEfficiency)) {
+            this.sailEfficiency = sailEfficiency;
+        } else {
+            throw new ArgumentException("Sail Effectiveness must be between [1...100].");
+        }
     }
 }
