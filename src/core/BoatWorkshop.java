884aa294673ba4241fb0e4d64bff5ed46068ce9a
SophYo <sophiya.dimitrova@gmail.com>
2019-07-30 23:51:48 +0300

    field added

--- src/core/BoatWorkshop.java
+++ src/core/BoatWorkshop.java
@@ -1,8 +1,17 @@
 package core;
 
 import models.Boat;
+import models.Engine;
+import repositories.Repository;
 
 public class BoatWorkshop implements BoatFactory {
+
+    private Repository<Engine> engineRepository;
+
+    public void setEngineRepository(Repository<Engine> engineRepository) {
+        this.engineRepository = engineRepository;
+    }
+
     @Override
     public Boat produce(String[] args) {
         return null;
