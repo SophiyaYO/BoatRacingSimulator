c64af971114019a643d8bbc7198e81af260eb143
SophYo <sophiya.dimitrova@gmail.com>
2019-08-03 16:19:56 +0300

    created + methods

---
+++ src/core/commands/boatCommands/CreateBoatEngine.java
@@ -0,0 +1,28 @@
+package core.commands.boatCommands;
+
+import core.EngineFactory;
+import core.commands.interfaces.Command;
+import exceptions.ArgumentException;
+import exceptions.DuplicateModelException;
+import models.Engine;
+import repositories.Repository;
+
+public class CreateBoatEngine implements Command {
+    public CreateBoatEngine(Repository<Engine> engineRepository, EngineFactory engineFactory) {
+        this.engineRepository = engineRepository;
+        this.engineFactory = engineFactory;
+    }
+
+
+    @Override
+    public String execute(String[] args) {
+        String result = null;
+        try {
+            this.engineRepository.add(this.engineFactory.produce(args));
+            result = String.format("Engine model %s with %s HP and displacement %s cm3 created successfully.", args[1], args[2], args[3]);
+        } catch (ArgumentException | DuplicateModelException ex) {
+            result = ex.getMessage();
+        }
+        return result;
+    }
+}
