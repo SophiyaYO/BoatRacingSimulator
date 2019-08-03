ddef1f3be408504915b11fe785f8c909ba31d12e
SophYo <sophiya.dimitrova@gmail.com>
2019-08-03 16:03:03 +0300

    setEngineRepository() removed

--- src/core/BoatFactory.java
+++ src/core/BoatFactory.java
@@ -3,11 +3,8 @@ package core;
 import exceptions.ArgumentException;
 import exceptions.NonExistanceModelException;
 import models.Boat;
-import models.Engine;
-import repositories.Repository;
 
 public interface BoatFactory {
     Boat produce(String[] args) throws ArgumentException, NonExistanceModelException;
 
-    void setEngineRepository(Repository<Engine> engineRepository);
 }
