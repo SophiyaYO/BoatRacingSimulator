84aa5bc7d7101da7da544799539f270db8f9ecf3
SophYo <sophiya.dimitrova@gmail.com>
2019-08-03 16:18:03 +0300

    methods implemented and fixed + constructor

--- src/core/CommandWorkshop.java
+++ src/core/CommandWorkshop.java
@@ -1,6 +1,7 @@
 package core;
 
 import core.commands.interfaces.Command;
+import core.controllers.interfaces.RaceManager;
 import models.Boat;
 import models.Engine;
 import repositories.Repository;
@@ -12,9 +13,45 @@ public class CommandWorkshop implements CommandFactory {
    private Repository<Boat> boatRepository;
    private RaceManager raceManager;
 
+    public CommandWorkshop(EngineFactory engineFactory, BoatFactory boatFactory,
+                           Repository<Engine> engineRepository, Repository<Boat> boatRepository,
+                           RaceManager raceManager) {
+        this.engineFactory = engineFactory;
+        this.boatFactory = boatFactory;
+        this.engineRepository = engineRepository;
+        this.boatRepository = boatRepository;
+        this.raceManager = raceManager;
+    }
+
+
+
 
     @Override
     public Command produce(String type) {
-        return null;
+        Command command = null;
+
+        if (type.equals("CreateBoatEngine")) {
+            command = new CreateBoatEngine(this.engineRepository, this.engineFactory);
+        } else if (type.equals("CreateRowBoat")) {
+            command = new CreateRowBoat(this.boatRepository, this.boatFactory);
+        } else if (type.equals("CreateSailBoat")) {
+            command = new CreateSailBoat(this.boatRepository, this.boatFactory);
+        } else if (type.equals("CreatePowerBoat")) {
+            command = new CreatePowerBoat(this.boatRepository, this.boatFactory);
+        } else if (type.equals("CreateYacht")) {
+            command = new CreateYacht(this.boatRepository, this.boatFactory);
+        } else if (type.equals("OpenRace")) {
+            command = new OpenRace(this.raceManager);
+        } else if (type.equals("SignUpBoat")) {
+            command = new SignUpBoat(this.raceManager, this.boatRepository);
+        } else if (type.equals("StartRace")) {
+            command = new StartRace(this.raceManager);
+        } else if (type.equals("GetStatistic")) {
+            command = new GetStatistic(this.raceManager);
+        }else if (type.equals("End")) {
+            command = new End();
+        }
+
+        return command;
     }
 }
