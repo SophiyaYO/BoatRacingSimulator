ca1fa1e898c2bbea0944d0d3c1d52df2bb7e9bf9
SophYo <sophiya.dimitrova@gmail.com>
2019-08-03 16:30:26 +0300

    created

---
+++ src/core/commands/raceCommands/OpenRace.java
@@ -0,0 +1,13 @@
+package core.commands.raceCommands;
+
+import core.RaceFactory;
+import core.commands.interfaces.Command;
+import core.controllers.interfaces.RaceManager;
+
+public class OpenRace extends RaceCommand {
+    private RaceFactory raceFactory;
+
+    public OpenRace(RaceManager raceManager) {
+
+    }
+}
